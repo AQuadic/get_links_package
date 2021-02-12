18d2cec5caa0e24a742e5b9135200c9e33817c74
Saifallak <Saifallak@yahoo.com>
2021-02-12 23:54:39 +0200

    refactor example project.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,108 +1,151 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get_links/get_links.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(AQGetLinksApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class AQGetLinksApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'AQ Get Link Package Example.',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: AQHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class AQHomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _AQHomePageState createState() => _AQHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _AQHomePageState extends State<AQHomePage> {
+  bool _isLoading = false;
+  String _fetchingLink;
+  List<String> _links = [];
 
-  Future<void> _incrementCounter() async {
-    print(await GetLinks()
-        .getByLink('http://www.mediafire.com/file/jy7i6s3ynop7bdc'));
+  Future<void> _fetch(BuildContext context, String val) async {
+    try {
+      // Enable Loading...
+      _fetchingLink = val;
+      _isLoading = true;
+      setState(() {});
+
+      // Delay One Sec. For no reason.
+      await Future.delayed(Duration(seconds: 1));
+
+      // Get Links.
+      _links = await AQFetcher().getByLink(link: val);
+
+      // Disable Loading...
+      _isLoading = false;
+      _fetchingLink = null;
+      setState(() {});
+    } catch (e) {
+      // Disable Loading...
+      _isLoading = false;
+      _fetchingLink = null;
+
+      // Add Error To Links.
+      _links = ['$e'];
+
+      // Rebuild Screen.
+      setState(() {});
+    }
+  }
+
+  String get _getSupportedWebsites => AQVideoWebsite.values
+      .map((e) => ' ${e.toString().split('AQVideoWebsite.')[1]} ')
+      .toString();
+
+  Widget _getBodyWidget(BuildContext context) {
+    if (_isLoading)
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(
+              '$_fetchingLink',
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 8),
+            CupertinoActivityIndicator(),
+          ],
+        ),
+      );
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          TextField(
+            autofillHints: <String>[AutofillHints.url],
+            keyboardAppearance: Brightness.dark,
+            keyboardType: TextInputType.url,
+            onSubmitted: (val) => _fetch(context, val),
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Website Url',
+              hintText: _getSupportedWebsites,
+              helperText: _links.isEmpty
+                  ? 'We Support Those Links: $_getSupportedWebsites'
+                  : '',
+              helperMaxLines: 5,
+              helperStyle: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+              hintStyle: TextStyle(
+                fontSize: 10,
+                fontWeight: FontWeight.bold,
+              ),
+              labelStyle: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: 24),
+          for (String i in _links) ...[
+            SizedBox(height: 4),
+            Text(
+              i,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
+              textScaleFactor: 1.0,
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 10,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Divider(),
+          ]
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        centerTitle: true,
+        title: Text('AQGetLink Example'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: _getBodyWidget(context),
     );
   }
 }
