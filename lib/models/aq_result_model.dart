21826994e07ef8a205810ddcce80c2a617f54b98
Saifallak <Saifallak@yahoo.com>
2021-04-26 22:37:42 +0200

    size

--- lib/models/aq_result_model.dart
+++ lib/models/aq_result_model.dart
@@ -2,11 +2,13 @@ class AQLinkResult {
   String url;
   String cookie;
   String quality;
+  String size;
 
   AQLinkResult.fromMap(Map<String, dynamic> map) {
     url = map['url'];
     cookie = map['cookie'];
     quality = map['quality'];
+    size = map['size'];
   }
 
   Map<String, dynamic> toMap() {
@@ -14,6 +16,7 @@ class AQLinkResult {
       'url': this.url,
       'cookie': this.cookie,
       'quality': this.quality,
+      'size': this.size,
     };
   }
 }
